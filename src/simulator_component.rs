fad99793bef57ed62e5b7338e0f2ddd732fc4cf7
GameHunter101 <liorscarmeli@gmail.com>
2024-08-09 00:30:00 -0400

    broken. velocity step doesnt work properly

--- src/simulator_component.rs
+++ src/simulator_component.rs
@@ -27,6 +27,77 @@ impl SimulatorComponent {
         }
     }
 
+    fn project(&mut self) {
+        let cell_dimension = 1.0 / self.resolution as f32;
+
+        for i in 1..=self.resolution {
+            for j in 1..=self.resolution {
+                self.velocity_grids[1_i32][i][j].y = -0.5
+                    * cell_dimension
+                    * (self.velocity_grids[0_i32][i + 1][j].x
+                        - self.velocity_grids[0_i32][i - 1][j].x
+                        + self.velocity_grids[0_i32][i][j + 1].y
+                        - self.velocity_grids[0_i32][i][j - 1].y);
+
+                self.velocity_grids[1_i32][i][j].x = 0.0;
+            }
+        }
+
+        Self::set_boundary(self.resolution, &mut self.velocity_grids[1_i32], true);
+
+        for _ in 0..20 {
+            for i in 1..=self.resolution {
+                for j in 1..=self.resolution {
+                    self.velocity_grids[1_i32][i][j].x = (self.velocity_grids[1_i32][i][j].y
+                        + self.velocity_grids[1_i32][i - 1][j].x
+                        + self.velocity_grids[1_i32][i + 1][j].x
+                        + self.velocity_grids[1_i32][i][j - 1].x
+                        + self.velocity_grids[1_i32][i][j + 1].x)
+                        / 4.0;
+                }
+            }
+            Self::set_boundary(self.resolution, &mut self.velocity_grids[1_i32], true);
+        }
+
+        for i in 1..=self.resolution {
+            for j in 1..=self.resolution {
+                self.velocity_grids[0_i32][i][j].x -= 0.5
+                    * (self.velocity_grids[1_i32][i + 1][j].x
+                        - self.velocity_grids[1_i32][i - 1][j].x)
+                    / cell_dimension;
+                self.velocity_grids[0_i32][i][j].y -= 0.5
+                    * (self.velocity_grids[1_i32][i][j + 1].x
+                        - self.velocity_grids[1_i32][i][j - 1].x)
+                    / cell_dimension;
+            }
+        }
+
+        Self::set_boundary(self.resolution, &mut self.velocity_grids[0_i32], false);
+    }
+
+    fn velocity_step(&mut self, dt: f32) {
+        Self::add_source(self.resolution, &mut self.velocity_grids, dt);
+        self.velocity_grids.rotate_left(1);
+        Self::diffuse(
+            self.resolution,
+            self.viscosity,
+            &mut self.velocity_grids,
+            dt,
+            true,
+        );
+        self.project();
+        self.velocity_grids.rotate_left(1);
+        let old_vels = self.velocity_grids[1_i32].clone();
+        Self::advect(
+            self.resolution,
+            &mut self.velocity_grids,
+            &old_vels,
+            dt,
+            false,
+        );
+        self.project();
+    }
+
     fn add_source<
         T: std::fmt::Debug + Clone + Copy + std::ops::Mul<f32, Output = T> + std::ops::AddAssign,
     >(
@@ -42,10 +113,18 @@ impl SimulatorComponent {
         }
     }
 
-    fn diffuse(
+    fn diffuse<
+        T: std::fmt::Debug
+            + Clone
+            + Copy
+            + std::ops::Mul<f32, Output = T>
+            + std::ops::AddAssign
+            + std::ops::Add<T, Output = T>
+            + std::ops::Div<f32, Output = T>,
+    >(
         resolution: usize,
         diffusion_coefficient: f32,
-        grid: &mut RingBuffer<Vec<Vec<f32>>>,
+        grid: &mut RingBuffer<Vec<Vec<T>>>,
         dt: f32,
         is_standard: bool,
     ) {
@@ -55,11 +134,11 @@ impl SimulatorComponent {
             for i in 1..=resolution {
                 for j in 1..=resolution {
                     grid[0_i32][i][j] = (grid[1_i32][i][j]
-                        + final_coefficient
-                            * (grid[0_i32][i - 1][j]
-                                + grid[0_i32][i + 1][j]
-                                + grid[0_i32][i][j - 1]
-                                + grid[0_i32][i][j + 1]))
+                        + (grid[0_i32][i - 1][j]
+                            + grid[0_i32][i + 1][j]
+                            + grid[0_i32][i][j - 1]
+                            + grid[0_i32][i][j + 1])
+                            * final_coefficient)
                         / (1.0 + 4.0 * final_coefficient);
                 }
             }
@@ -68,9 +147,17 @@ impl SimulatorComponent {
         }
     }
 
-    fn advect(
+    fn advect<
+        T: std::fmt::Debug
+            + Clone
+            + Copy
+            + std::ops::Mul<f32, Output = T>
+            + std::ops::AddAssign
+            + std::ops::Add<T, Output = T>
+            + std::ops::Div<f32, Output = T>,
+    >(
         resolution: usize,
-        grid: &mut RingBuffer<Vec<Vec<f32>>>,
+        grid: &mut RingBuffer<Vec<Vec<T>>>,
         velocity: &[Vec<Vector2<f32>>],
         dt: f32,
         is_standard: bool,
@@ -98,57 +185,80 @@ impl SimulatorComponent {
                 let t_1 = vector.y - j_0 as f32;
                 let t_0 = 1.0 - t_1;
 
-                grid[0_i32][i][j] = s_0
-                    * (t_0 * grid[1_i32][i_0][j_0] + t_1 * grid[1_i32][i_0][j_1])
-                    + s_1 * (t_0 * grid[1_i32][i_1][j_0] + t_1 * grid[1_i32][i_1][j_1]);
+                grid[0_i32][i][j] = (grid[1_i32][i_0][j_0] * t_0 + grid[1_i32][i_0][j_1] * t_1)
+                    * s_0
+                    + (grid[1_i32][i_1][j_0] * t_0 + grid[1_i32][i_1][j_1] * t_1) * s_1;
             }
         }
 
         Self::set_boundary(resolution, &mut grid[0_i32], is_standard)
     }
 
-    fn set_boundary(resolution: usize, grid: &mut [Vec<f32>], is_standard: bool) {
+    fn set_boundary<
+        T: std::fmt::Debug
+            + Clone
+            + Copy
+            + std::ops::Mul<f32, Output = T>
+            + std::ops::AddAssign
+            + std::ops::Add<T, Output = T>,
+    >(
+        resolution: usize,
+        grid: &mut [Vec<T>],
+        is_standard: bool,
+    ) {
         for i in 1..=resolution {
-            grid[0][i] = if is_standard { grid[1][i] } else { -grid[1][i] };
+            grid[0][i] = if is_standard {
+                grid[1][i]
+            } else {
+                grid[1][i] * -1.0
+            };
             grid[resolution + 1][i] = if is_standard {
                 grid[resolution][i]
             } else {
-                -grid[resolution][i]
+                grid[resolution][i] * -1.0
             };
 
-            grid[i][0] = if is_standard { grid[i][1] } else { -grid[i][1] };
+            grid[i][0] = if is_standard {
+                grid[i][1]
+            } else {
+                grid[i][1] * -1.0
+            };
             grid[i][resolution + 1] = if is_standard {
                 grid[i][resolution]
             } else {
-                -grid[i][resolution]
+                grid[i][resolution] * -1.0
             };
         }
 
-        grid[0][0] = 0.5 * (grid[1][0] + grid[0][1]);
-        grid[0][resolution + 1] = 0.5 * (grid[1][resolution + 1] + grid[0][resolution]);
-        grid[resolution + 1][0] = 0.5 * (grid[resolution][0] + grid[resolution + 1][1]);
+        grid[0][0] = (grid[1][0] + grid[0][1]) * 0.5;
+        grid[0][resolution + 1] = (grid[1][resolution + 1] + grid[0][resolution]) * 0.5;
+        grid[resolution + 1][0] = (grid[resolution][0] + grid[resolution + 1][1]) * 0.5;
         grid[resolution + 1][resolution + 1] =
-            0.5 * (grid[resolution][resolution + 1] + grid[resolution + 1][resolution]);
+            (grid[resolution][resolution + 1] + grid[resolution + 1][resolution]) * 0.5;
     }
 
     fn density_step(&mut self, dt: f32) {
+        // println!("{}", self.density_grids[0_i32][50][50]);
         Self::add_source(self.resolution, &mut self.density_grids, dt);
+        // println!("{}", self.density_grids[0_i32][50][50]);
         self.density_grids.rotate_left(1);
         Self::diffuse(
             self.resolution,
             self.diffusion_coefficient,
             &mut self.density_grids,
             dt,
-            false,
+            true,
         );
+        // println!("{}", self.density_grids[0_i32][50][50]);
         self.density_grids.rotate_left(1);
         Self::advect(
             self.resolution,
             &mut self.density_grids,
             &self.velocity_grids[0_i32],
             dt,
-            false,
+            true,
         );
+        // println!("{}", self.density_grids[0_i32][50][50]);
     }
 }
 
@@ -166,9 +276,15 @@ impl ComponentSystem for SimulatorComponent {
         for i in 0..10 {
             for j in 0..10 {
                 self.density_grids[0_i32][50 + i][50 + j] = 1.0;
-                self.velocity_grids[0_i32][20 + i][20 + j] = Vector2::new(0.0, 1.0);
+                self.velocity_grids[0_i32][50 + i][50 + j] = Vector2::new(1.0, 0.0);
             }
         }
+
+        /* for i in 1..50 {
+            for j in 1..=self.resolution {
+                self.velocity_grids[0_i32][i][j] = Vector2::new(1.0, 0.0);
+            }
+        } */
     }
 
     fn update(
@@ -187,6 +303,19 @@ impl ComponentSystem for SimulatorComponent {
         let details = engine_details.lock().unwrap();
         let dt = details.last_frame_duration.as_secs_f32();
 
+        /* for i in 0..10 {
+            for j in 0..10 {
+                self.density_grids[0_i32][50 + i][50 + j] = 1.0;
+            }
+        } */
+
+        for i in 1..50 {
+            for j in 1..=self.resolution {
+                self.velocity_grids[0_i32][i][j] = Vector2::new(1.0, 0.0);
+            }
+        }
+
+        self.velocity_step(dt);
         self.density_step(dt);
 
         let materials = materials.unwrap();
@@ -195,13 +324,18 @@ impl ComponentSystem for SimulatorComponent {
             let grid_2: [[f32; 128]; 128] = self.density_grids[0_i32][1..=self.resolution]
                 .iter()
                 .map(|row| {
-                    let arr: [f32; 128] = row.clone()[1..=self.resolution].try_into().unwrap();
+                    let arr: [f32; 128] = row.clone()[1..=self.resolution]
+                        /* .iter()
+                        .map(|v| v.magnitude())
+                        .collect::<Vec<_>>() */
+                        .try_into()
+                        .unwrap();
                     arr
                 })
                 .collect::<Vec<_>>()
                 .try_into()
                 .unwrap();
-            println!("{:?}", &grid_2[50][50]);
+            // println!("{:?}", &grid_2[20][20]);
             queue.write_buffer(buffer, 0, bytemuck::cast_slice(&[grid_2]));
         }
     }
